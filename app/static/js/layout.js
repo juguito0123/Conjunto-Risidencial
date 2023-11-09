3cba390f063054d1024b358adbcddd18b356e7b5
David Santiago Giraldo Suescun <134398515+juguito0123@users.noreply.github.com>
2023-11-08 22:55:08 -0500

    si

--- app/static/js/layout.js
+++ app/static/js/layout.js
@@ -7,3 +7,41 @@ document.addEventListener("DOMContentLoaded", function() {
       form.submit();
     });
   });
+
+  document.getElementById("personaNatural").addEventListener("change", function() {
+    document.getElementById("naturalForm").style.display = "block";
+    document.getElementById("juridicaForm").style.display = "none";
+});
+
+document.getElementById("personaJuridica").addEventListener("change", function() {
+    document.getElementById("naturalForm").style.display = "none";
+    document.getElementById("juridicaForm").style.display = "block";
+});
+
+function agregarPersona() {
+    // Lógica para agregar persona según el formulario visible
+    const tipoPersona = document.querySelector('input[name="tipoPersona"]:checked').value;
+
+    if (tipoPersona === 'natural') {
+        // Lógica para agregar persona natural
+        const tipoIdentificacion = document.getElementById("tipoIdentificacion").value;
+        // Resto de la lógica para persona natural
+    } else {
+        // Lógica para agregar persona jurídica
+        const razonSocial = document.getElementById("razonSocial").value;
+        // Resto de la lógica para persona jurídica
+    }
+
+    // Actualizar la lista en la interfaz de usuario
+    mostrarPersonasRegistradas();
+
+    // Limpiar el formulario
+    document.getElementById("personForm").reset();
+}
+
+function mostrarPersonasRegistradas() {
+    const personList = document.getElementById("personList");
+    personList.innerHTML = "";
+
+    // Lógica para mostrar personas registradas
+}
